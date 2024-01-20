3a9ae0ad61ed5e3d7d031326a88990fb289cddb2
Aleksander Krajewski <116189519+Porkelson@users.noreply.github.com>
2024-01-20 18:37:56 +0100

    player footsteps

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,18 +12,39 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 moveVector;
     private Rigidbody2D rb;
 
+
+    public AudioClip footstepSound; // Array to hold footstep sound clips
+    public float minTimeBetweenFootsteps = 0.3f; // Minimum time between footstep sounds
+    public float maxTimeBetweenFootsteps = 0.6f; // Maximum time between footstep sounds
+
+    private AudioSource audioSource; // Reference to the Audio Source component
+    private bool isWalking = false; // Flag to track if the player is walking
+    private float timeSinceLastFootstep; // Time since the last footstep sound
+
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         moveVector = context.ReadValue<Vector2>();
+
+        // Check if enough time has passed to play the next footstep sound
+        if (Time.time - timeSinceLastFootstep >= Random.Range(minTimeBetweenFootsteps, maxTimeBetweenFootsteps))
+        {
+            // Play a random footstep sound from the array
+            audioSource.PlayOneShot(footstepSound);
+
+            timeSinceLastFootstep = Time.time; // Update the time since the last footstep sound
+        }
     }
 
     private void Update()
     {
         rb.velocity = speed * (Vector3)moveVector;
+        
     }
 }

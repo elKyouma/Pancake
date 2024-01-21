db933a9cd9001414b00fa37d908b638dec6987cc
kapiw04 <wojtowicz.kacper@gmail.com>
2024-01-21 05:49:49 +0100

    pull

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 using TMPro;
 using Unity.VisualScripting;
 
-public class Timer : MonoBehaviour {
+public class Timer : MonoBehaviour
+{
     [SerializeField]
     private float timerDuration = 3f * 60f; //Duration of the timer in seconds
 
@@ -31,39 +32,53 @@ public class Timer : MonoBehaviour {
 
     private UIDetectedCutscene cutscene;
 
-    private void Start() {
+    private void Start()
+    {
         ResetTimer();
         cutscene = transform.parent.GetComponent<UIDetectedCutscene>();
     }
 
-    private void ResetTimer() {
-        if (countDown) {
+    private void ResetTimer()
+    {
+        if (countDown)
+        {
             timer = timerDuration;
-        } else {
+        }
+        else
+        {
             timer = 0;
         }
         SetTextDisplay(true);
     }
 
-    void Update() {
-        if (countDown && timer > 0) {
+    void Update()
+    {
+        if (countDown && timer > 0)
+        {
             timer -= Time.deltaTime;
             UpdateTimerDisplay(timer);
-        } else if (!countDown && timer < timerDuration) {
+        }
+        else if (!countDown && timer < timerDuration)
+        {
             timer += Time.deltaTime;
             UpdateTimerDisplay(timer);
-        } else {
+        }
+        else
+        {
             FlashTimer();
             cutscene.Activate();
         }
     }
 
-    private void UpdateTimerDisplay(float time) {
-        if (time < 0) {
+    private void UpdateTimerDisplay(float time)
+    {
+        if (time < 0)
+        {
             time = 0;
         }
 
-        if (time > 3660) {
+        if (time > 3660)
+        {
             Debug.LogError("Timer cannot display values above 3660 seconds");
             ErrorDisplay();
             return;
@@ -82,7 +97,8 @@ public class Timer : MonoBehaviour {
         //timerText.text = currentTime.ToString();
     }
 
-    private void ErrorDisplay() {
+    private void ErrorDisplay()
+    {
         firstMinute.text = "8";
         secondMinute.text = "8";
         firstSecond.text = "8";
@@ -93,29 +109,38 @@ public class Timer : MonoBehaviour {
         //timerText.text = "ERROR";
     }
 
-    private void FlashTimer() {
-        if(countDown && timer != 0) {
+    private void FlashTimer()
+    {
+        if (countDown && timer != 0)
+        {
             timer = 0;
             UpdateTimerDisplay(timer);
         }
 
-        if(!countDown && timer != timerDuration) {
+        if (!countDown && timer != timerDuration)
+        {
             timer = timerDuration;
             UpdateTimerDisplay(timer);
         }
 
-        if(flashTimer <= 0) {
+        if (flashTimer <= 0)
+        {
             flashTimer = flashDuration;
-        } else if (flashTimer <= flashDuration / 2) {
+        }
+        else if (flashTimer <= flashDuration / 2)
+        {
             flashTimer -= Time.deltaTime;
             SetTextDisplay(true);
-        } else {
+        }
+        else
+        {
             flashTimer -= Time.deltaTime;
             SetTextDisplay(false);
         }
     }
 
-    private void SetTextDisplay(bool enabled) {
+    private void SetTextDisplay(bool enabled)
+    {
         firstMinute.enabled = enabled;
         secondMinute.enabled = enabled;
         separator.enabled = enabled;
--- Assets/UIDetectedCutscene.cs
+++ Assets/UIDetectedCutscene.cs
@@ -43,4 +43,3 @@ public class UIDetectedCutscene : MonoBehaviour
         vc.m_Lens.OrthographicSize = newSize;
     }
 }
- 
